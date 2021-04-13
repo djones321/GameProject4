1e1721f135cd607a60f8fafb433153a974f81e30
djones321 <damonjones321@ksu.edu>
2021-04-12 22:06:10 -0500

    Initial Commit

---
+++ PixieParticleSystem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace GameProject4
+{
+    class PixieParticleSystem : ParticleSystem
+    {
+        IParticleEmitter _emitter;
+        float scale;
+
+        public PixieParticleSystem(Game game, IParticleEmitter emitter) : base(game, 2000)
+        {
+            _emitter = emitter;
+        }
+
+        protected override void InitializeConstants()
+        {
+            textureFilename = "particle";
+            minNumParticles = 2;
+            maxNumParticles = 5;
+
+            blendState = BlendState.Additive;
+            DrawOrder = AdditiveBlendDrawOrder;
+        }
+
+        protected override void InitializeParticle(ref Particle p, Vector2 where)
+        {
+            var velocity = _emitter.Velocity;
+
+            var acceleration = Vector2.UnitY * 400;
+
+            scale = RandomHelper.NextFloat(0.1f, 0.5f);
+
+            var lifetime = RandomHelper.NextFloat(0.1f, 1.0f);
+
+            p.Initialize(where, velocity, acceleration, Color.Goldenrod, scale: scale, lifetime: lifetime);
+            
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            this.InitializeConstants();
+            base.Update(gameTime);
+            AddParticles(_emitter.Position);
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Screens/MainMenuScreen.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using GameProject4.StateManagement;
+
+namespace GameProject4.Screens
+{
+    // The main menu screen is the first thing displayed when the game starts up.
+    public class MainMenuScreen : MenuScreen
+    {
+        public float volume=10;
+        OptionsMenuScreen options = new OptionsMenuScreen();
+        public Game game;
+
+        public MainMenuScreen(Game game) : base("Main Menu")
+        {
+            var playGameMenuEntry = new MenuEntry("Play Game");
+            var optionsMenuEntry = new MenuEntry("Options");
+            var exitMenuEntry = new MenuEntry("Exit");
+
+            this.game = game;
+
+            playGameMenuEntry.Selected += PlayGameMenuEntrySelected;
+            optionsMenuEntry.Selected += OptionsMenuEntrySelected;
+            exitMenuEntry.Selected += OnCancel;
+
+            MenuEntries.Add(playGameMenuEntry);
+            MenuEntries.Add(optionsMenuEntry);
+            MenuEntries.Add(exitMenuEntry);
+        }
+
+        private void PlayGameMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            volume = options.volume;
+            LoadingScreen.Load(ScreenManager, true, e.PlayerIndex, new GameplayScreen(volume, game));//, new CutSceneScreen());
+        }
+
+        private void OptionsMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            ScreenManager.AddScreen(options, e.PlayerIndex);
+        }
+
+        protected override void OnCancel(PlayerIndex playerIndex)
+        {
+            const string message = "Are you sure you want to exit this sample?";
+            var confirmExitMessageBox = new MessageBoxScreen(message);
+
+            confirmExitMessageBox.Accepted += ConfirmExitMessageBoxAccepted;
+
+            ScreenManager.AddScreen(confirmExitMessageBox, playerIndex);
+        }
+
+        private void ConfirmExitMessageBoxAccepted(object sender, PlayerIndexEventArgs e)
+        {
+            ScreenManager.Game.Exit();
+        }
+    }
+}
